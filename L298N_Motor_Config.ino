dafdf9f6c1b90284fdbe8fd02df45ae869bfcf86
Sarfraz Nawaz <78136256+informsarfu@users.noreply.github.com>
2021-10-11 10:09:43 +0530

    Add files via upload

---
+++ L298N_Motor_Config.ino
@@ -0,0 +1,45 @@
+void setup() {
+Serial.begin (9600); //Starting the serial communication at 9600 baud rate
+pinMode(IN1, OUTPUT);  //Initializing the motor pins as output 
+pinMode(IN2, OUTPUT);
+pinMode(IN3, OUTPUT);
+pinMode(IN4, OUTPUT);
+ 
+//Initializng the joystick pins as input
+pinMode (7, INPUT) ;
+pinMode(13,OUTPUT);                    
+
+}
+
+void loop() {
+ Serial.print("IRSensorip  ");
+Serial.println(digitalRead(7));
+if (digitalRead(7)==0){     //Rotating the left motor in clockwise direction
+//Mapping the values to 0-255 to move the motor
+digitalWrite(IN1, HIGH);
+digitalWrite(IN2, HIGH);
+
+delay(1500);
+Serial.print("IRSensorip  ");
+Serial.println(digitalRead(7));
+
+}
+ Serial.print("IRSensorip  ");
+Serial.println(digitalRead(7));
+if(digitalRead(7)==0){     //Rotating the left motor in clockwise direction
+//Mapping the values to 0-255 to move the motor
+digitalWrite(IN3, HIGH);
+digitalWrite(IN4, HIGH);
+Serial.print("IRSensorip  ");
+Serial.println(digitalRead(7));
+}
+ else {  //Motors will not move when the joystick will be at center
+digitalWrite(IN1, LOW);
+digitalWrite(IN2, LOW);
+digitalWrite(IN3, LOW);
+digitalWrite(IN4, LOW);
+Serial.print("IRSensorip  ");
+Serial.println(digitalRead(7));
+}
+ delay(1000);
+}
